4814c4c6efe7354f7ebd8b1017aed66b1912dca0
Кристина Кюркчиева <s45523@astero.openfmi.net>
2024-05-23 13:26:50 +0300

    task 13 solved

--- OS-C-Tasks/task13/main.c
+++ OS-C-Tasks/task13/main.c
@@ -10,8 +10,9 @@ int main(int argc, char* argv[]) {
 		err(1, "Usage: <command> <command> <command>\n");
 		return -1;
 	}
-	//int status;
-	pid_t firstChild, secondChild;
+
+	int status;
+	pid_t firstChild;
 
 	firstChild = fork();
 	if(firstChild < 0)
@@ -20,51 +21,58 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	secondChild = fork();
-	if(secondChild < 0)
+	if(firstChild > 0)
 	{
-		err(1, "system call fork() was NOT successfull\n");
-		return -1;
-	}
+		
+		pid_t secondChild = fork();
+		if(secondChild < 0)
+		{
+			err(1, "system call fork() was NOT successfull\n");
+			return -1;
+		}
+	
 
-	if( firstChild > 0 && secondChild > 0)
-	{
+		if(secondChild > 0)
+		{
 		//parent
-		siginfo_t info;
-		waitid(P_ALL, 0, &info, WEXITED);
-		if(WEXITED)
-		{
+			pid_t firstExited = wait(&status);
 			char buffer[256];
-			int length = snprintf(buffer, sizeof(buffer), "%d\n", info.si_pid) ;
-			if( write(1, buffer, length) != length)
+			if(WIFEXITED(status))
 			{
-				err(2, "Writing was not successful\n");
+				if(WEXITSTATUS(status) == 0)
+				{
+					int length = snprintf(buffer, sizeof(buffer), "%d\n", firstExited) ;
+					if( write(1, buffer, length) != length)
+					{
+						err(2, "Writing was NOT successful\n");
+					}
+				}
+			}
+			else
+			{
+				if( write(1, "-1", strlen("-1")) != strlen("-1"))
+				{
+					err(2, "Writing was NOT successfull\n");
+				}
 			}
 		}
-		else
+		if(secondChild == 0)
 		{
-			if( write(1, "-1", strlen("-1")) != strlen("-1"))
+			if( execlp(argv[2],argv[2], (char*)NULL) == -1)
 			{
-				err(2, "Writing was NOT successfull\n");
+				err(2, "Execution of %s was NOT successful\n", argv[2]);
+				return -1;
 			}
 		}
-
 	}
-	else if( firstChild == 0)
+	if( firstChild == 0)
 	{
-		if( execlp(argv[1],argv[1], (char*)NULL) == -1)
+		if( execlp(argv[1], argv[1], (char*)NULL) == -1)
 		{
 			err(2, "Execution of %s was NOT successful\n", argv[1]);
-		}
-	}
-	else if( secondChild == 0)
-	{
-		if( execlp(argv[2], argv[2], (char*)NULL) == -1)
-		{
-			err(2, "Execution of %s was NOT successful\n", argv[2]);
+			return -1;
 		}	
 	}
-
 
 	return 0;
 }
